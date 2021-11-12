bede2d7fe9aa11f2bcb48deb365916feb17ebf72
Ivan <81901071+ivanus44@users.noreply.github.com>
2021-11-12 19:24:51 +0300

    ТЕОРИЯ 3

---
+++ heart3.py
@@ -0,0 +1,46 @@
+import pandas as pd
+df = pd.read_csv('heart_disease_health.csv')
+
+df.drop(['HighBP','HighChol','CholCheck','BMI','Smoker','Sex','Stroke','Diabetes','PhysActivity','Fruits','Veggies','HvyAlcoholConsump','AnyHealthcare','NoDocbcCost','GenHlth','MentHlth','PhysHlth','DiffWalk','Education','Income'],axis = 1, inplace = True)
+print('после 30 инфаркт случается чаще')
+def fill_int(Age):
+    if Age <= 30:
+        return 1
+    return 0
+df['Age'] = df['Age'].apply(fill_int)
+def fill_HDA(HDA):
+    if HDA == 1.0:
+        return 1
+    return 0
+df['HeartDiseaseorAttack'] = df['HeartDiseaseorAttack'].apply(fill_HDA)
+print(df)
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+
+X = df.drop('HeartDiseaseorAttack', axis = 1)
+y = df['HeartDiseaseorAttack']
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.25)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = KNeighborsClassifier(n_neighbors = 5)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print('Процент правильного предсказания',accuracy_score(y_test, y_pred) * 100)
+print('Confusion matrix')
+print(confusion_matrix(y_test, y_pred))
+
+
+import matplotlib.pyplot as plt
+
+s = pd.Series(data = [212,186], index = ['ДО 30','ПОСЛЕ 30'])
+s.plot(kind = 'barh')
+
+plt.show()
